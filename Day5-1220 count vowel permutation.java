c25e01fb2d9a42e299e664c070c3162440474d78
Muthuprabha <muthuprabha463@gmail.com>
2026-07-22 08:30:46 +0530

    Create Day5-1220 count vowel permutation.java

---
+++ Day5-1220 count vowel permutation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countVowelPermutation(int n) {
+        long mod = 1000000007;
+
+        long a = 1, e = 1, i = 1, o = 1, u = 1;
+
+        for (int k = 2; k <= n; k++) {
+            long na = (e + i + u) % mod;
+            long ne = (a + i) % mod;
+            long ni = (e + o) % mod;
+            long no = i % mod;
+            long nu = (i + o) % mod;
+
+            a = na;
+            e = ne;
+            i = ni;
+            o = no;
+            u = nu;
+        }
+
+        return (int)((a + e + i + o + u) % mod);
+    }
+}
